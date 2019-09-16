fa71171c155ba2fab935aee64e322d4bd1a09c88
chayeongrong <chayl0814@hatiolab.com>
2019-09-16 12:33:59 +0900

    주문 서비스 / DB 추가

--- server/routes.ts
+++ server/routes.ts
@@ -6,14 +6,47 @@ process.on('bootstrap-module-route' as any, (app, routes) => {
   const bodyParserOption = {
     formLimit: '10mb',
     jsonLimit: '10mb',
-    textLimit: '10mb'
+    textLimit: '10mb',
+    extendTypes: {
+      text: ['application/xml']
+    }
   }
 
-  routes.post('/create_order', koaBodyParser(), async (context, next) => {
+  routes.post('/create_order', koaBodyParser(bodyParserOption), async (context, next) => {
     try {
       const order = context.request.body
       const newOrder = await getRepository(Order).save(order)
       return newOrder
+    } catch (e) {
+      context.status = 401
+      context.body = {
+        text: context,
+        message: e.message
+      }
+    }
+  })
+
+  routes.get('/get_order/:model', async (context, next) => {
+    try {
+      const order = context.request.body
+      const getOrder = await getRepository(Order).find(order)
+      // context.type = 'text/plain'
+      return getOrder
+    } catch (e) {
+      context.status = 401
+      context.body = {
+        text: context,
+        message: e.message
+      }
+    }
+  })
+
+  routes.post('/reset_order', async (context, next) => {
+    try {
+      const order = context.request.body
+      await getRepository(Order).remove(order)
+      context.type = 'text/plain'
+      return 'Complete'
     } catch (e) {
       context.status = 401
       context.body = {
