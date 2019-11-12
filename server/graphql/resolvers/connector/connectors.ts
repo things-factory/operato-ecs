c9a3078cd866840b262522fe856e2f732f51d858
Hearty Oh <heartyoh@hatiolab.com>
2019-11-12 19:33:18 +0900

    Update connectors.ts

--- server/graphql/resolvers/connector/connectors.ts
+++ server/graphql/resolvers/connector/connectors.ts
@@ -2,7 +2,7 @@ import { Connections } from '../../../engine/connections'
 
 export const connectorsResolver = {
   connectors(_: any, {}, context: any) {
-    var connectors = Connections.getConnectors
+    var connectors = Connections.getConnectors()
     var items = Object.keys(connectors).map(name => {
       return {
         name,
