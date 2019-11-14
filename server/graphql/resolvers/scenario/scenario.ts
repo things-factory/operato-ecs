631285f2e017dc6a2662c33b491df5f97a3cd5b9
lllleeee <lcj1123@hatiolab.com>
2019-11-14 11:28:55 +0900

    Update scenario.ts

--- server/graphql/resolvers/scenario/scenario.ts
+++ server/graphql/resolvers/scenario/scenario.ts
@@ -4,8 +4,8 @@ import { Scenario } from '../../../entities'
 export const scenarioResolver = {
   async scenario(_: any, { id }, context: any) {
     return await getRepository(Scenario).findOne({
-      where: { id },
-      relations: ['steps', 'creator', 'updater']
+      where: { domain: context.state.domain, id },
+      relations: ['domain', 'steps', 'creator', 'updater']
     })
   }
 }
