4a74fc8f2e373c2907bcc7b8c5768562d161c31a
chunseop <chunseop@gmail.com>
2020-01-22 10:23:21 +0900

    Update index.ts

--- server/graphql/types/index.ts
+++ server/graphql/types/index.ts
@@ -1,27 +1,35 @@
 import * as Sheet from './sheet'
-import * as Order from './order'
 import * as Stock from './stock'
 import * as BoardSetting from './board-setting'
+import * as Product from './product'
+import * as SaleOrder from './sale-order'
+import * as WorkOrder from './work-order'
 
 
 export const queries = [
   Sheet.Query,
-  Order.Query,
   Stock.Query,
   BoardSetting.Query,
+  Product.Query,
+  SaleOrder.Query,
+  WorkOrder.Query,
 ]
 
 export const mutations = [
   Sheet.Mutation,
-  Order.Mutation,
   Stock.Mutation,
+  Product.Mutation,
+  SaleOrder.Mutation,
+  WorkOrder.Mutation,
 ]
 
 // export const subscriptions = [Scenario.Subscription]
 
 export const types = [
   ...Sheet.Types,
-  ...Order.Types,
   ...Stock.Types,
   ...BoardSetting.Types,
+  ...Product.Types,
+  ...SaleOrder.Types,
+  ...WorkOrder.Types,
 ]
