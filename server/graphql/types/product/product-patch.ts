5bb0e3cf5dac9f093da1708ecda0e43ccb57779f
chunseop <chunseop@gmail.com>
2020-01-29 17:57:02 +0900

    Update product-patch.ts

--- server/graphql/types/product/product-patch.ts
+++ server/graphql/types/product/product-patch.ts
@@ -2,9 +2,9 @@ import gql from 'graphql-tag'
 
 export const ProductPatch = gql`
   input ProductPatch {
-    id: String
-    code: String!
-    name: String!
+    id: String!
+    code: String
+    name: String
     description: String
     type: String
     active: Boolean
