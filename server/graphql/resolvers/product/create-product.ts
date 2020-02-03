1810c1191a9d17adb551727d04a7f7852de4ea54
chunseop <chunseop@gmail.com>
2020-02-03 11:56:01 +0900

    delete uuid()

--- server/graphql/resolvers/product/create-product.ts
+++ server/graphql/resolvers/product/create-product.ts
@@ -1,5 +1,3 @@
-import uuid from 'uuid/v4'
-
 import { getRepository } from 'typeorm'
 import { Product } from '../../../entities'
 
@@ -7,7 +5,6 @@ export const createProduct = {
   async createProduct(_, { product: attrs }, context: any) {
     const repository = getRepository(Product)
     const newProduct = {
-      id: uuid(),
       domain: context.state.domain,
       creator: context.state.user,
       updater: context.state.user,
--- server/graphql/resolvers/sale-order/create-sale-order.ts
+++ server/graphql/resolvers/sale-order/create-sale-order.ts
@@ -1,5 +1,3 @@
-import uuid from 'uuid/v4'
-
 import { getRepository } from 'typeorm'
 import { SaleOrder } from '../../../entities'
 
@@ -7,7 +5,6 @@ export const createSaleOrder = {
   async createSaleOrder(_, { saleOrder: attrs }, context: any) {
     const repository = getRepository(SaleOrder)
     const newSaleOrder = {
-      id: uuid(),
       domain: context.state.domain,
       creator: context.state.user,
       updater: context.state.user,
