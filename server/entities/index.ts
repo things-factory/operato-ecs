07489e1c18ac2463aaee20d59709bb34a00f6f75
chunseop <chunseop@gmail.com>
2020-02-06 15:40:32 +0900

    robert-chicken관련 파일 이동

--- server/entities/index.ts
+++ server/entities/index.ts
@@ -1,13 +1,5 @@
 import { Sheet } from './sheet'
-import { Product } from './product'
-import { Stock } from './stock'
 
-import { SaleOrder } from './sale-order'
-import { SaleOrderDetail } from './sale-order-detail'
-import { WorkOrder } from './work-order'
-import { Tool } from './tool'
+export const entities = [Sheet]
 
-
-export const entities = [Sheet, Stock, Product, SaleOrder, SaleOrderDetail, WorkOrder, Tool]
-
-export { Sheet, Stock, Product, SaleOrder, SaleOrderDetail, WorkOrder, Tool }
+export { Sheet }
