8e867936b39556ad1092e06994b1f1990a9c0876
Hearty Oh <heartyoh@hatiolab.com>
2020-07-28 00:40:45 +0900

    Update ecs-board-viewer-page.js

--- client/pages/board/ecs-board-viewer-page.js
+++ client/pages/board/ecs-board-viewer-page.js
@@ -3,15 +3,7 @@ import { BoardViewerPage } from '@things-factory/board-ui'
 export class ECSBoardViewerPage extends BoardViewerPage {
   get context() {
     return {
-      title: this.lifecycle.params['title'] || super.context.title,
-      printable: {
-        accept: ['preview'],
-        content: async () => {
-          await this.printTrick()
-
-          return this
-        }
-      }
+      title: this.lifecycle.params['title'] || super.context.title
     }
   }
 }
