22bcb5746c98ad76ef3e2b4ed4f307b469ae598b
Hearty Oh <heartyoh@hatiolab.com>
2020-07-28 00:39:57 +0900

    upgrade grist-ui

--- client/pages/board/ecs-board-viewer-page.js
+++ client/pages/board/ecs-board-viewer-page.js
@@ -3,7 +3,15 @@ import { BoardViewerPage } from '@things-factory/board-ui'
 export class ECSBoardViewerPage extends BoardViewerPage {
   get context() {
     return {
-      title: this.lifecycle.params['title'] || super.context.title
+      title: this.lifecycle.params['title'] || super.context.title,
+      printable: {
+        accept: ['preview'],
+        content: async () => {
+          await this.printTrick()
+
+          return this
+        }
+      }
     }
   }
 }
